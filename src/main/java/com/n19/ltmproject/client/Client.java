a2cfe738225f12126c5e9c4b0b3932f73d650860
deltaDC <ducchinh.work@gmail.com>
2024-11-02 16:52:55 +0700

    ref #73 and #83 add gameplay simple css, and auto logout when close app

--- src/main/java/com/n19/ltmproject/client/Client.java
+++ src/main/java/com/n19/ltmproject/client/Client.java
@@ -1,17 +1,24 @@
 package com.n19.ltmproject.client;
 
 import com.n19.ltmproject.client.handler.ServerHandler;
+import com.n19.ltmproject.client.model.Player;
+import com.n19.ltmproject.client.model.auth.SessionManager;
+import com.n19.ltmproject.client.service.MessageService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import javafx.scene.Parent;
+import javafx.stage.WindowEvent;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
-
 public class Client extends Application {
+    private final ServerHandler serverHandler = ServerHandler.getInstance();
+    private final MessageService messageService = new MessageService(serverHandler);
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -25,7 +32,6 @@ public class Client extends Application {
             System.out.println("Failed to connect to the server: " + e.getMessage());
         }
 
-
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/n19/ltmproject/Login.fxml")));
             Scene scene = new Scene(root);
@@ -38,9 +44,19 @@ public class Client extends Application {
             scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/n19/ltmproject/css/ExitBattle.css")).toExternalForm());
             scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/n19/ltmproject/css/Result.css")).toExternalForm());
             primaryStage.setScene(scene);
+
+            primaryStage.setOnCloseRequest(this::handleClose);
+
             primaryStage.show();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void handleClose(WindowEvent event) {
+        Player currentUser = SessionManager.getCurrentUser();
+        Map<String, Object> params = new HashMap<>();
+        params.put("username", currentUser.getUsername());
+        messageService.sendRequestNoResponse("logout", params);
     }
 }
